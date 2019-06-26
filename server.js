4e35165abd249a7a4875cdbd4f302287a35bf668
Mahmoud Khader <mahmoudaminkhader@gmail.com>
2019-06-25 23:29:56 -0400

    localhost fix for deployment to netlify

--- server.js
+++ server.js
@@ -100,7 +100,6 @@ app.get("*", (req, res) => {
 
 // const port = process.env.PORT || 9000;
 const port = 9000;
-app.listen(port);
 
 // Pass in the port that we want the app to listen to
 app.listen(port, () => console.log(`Server running on port ${port}`));
